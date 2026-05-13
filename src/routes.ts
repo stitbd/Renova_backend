266d9fcf3e9c24331a729a4e0af1304c37f97022
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2026-05-13 14:40:42 +0600

    at

--- src/routes.ts
+++ src/routes.ts
@@ -5,13 +5,10 @@ import { doctorRoutes } from "./modules/doctor/doctor.route";
 import { doctorDocumentRoutes } from "./modules/doctor_documents/doctor_documents.route";
 import { doctorSpecializationRoutes } from "./modules/doctor_specialization/doctor_specialization.route";
 import { doctorScheduleRoutes } from "./modules/doctor_schedule/doctor_schedule.route";
-import { patientAuthRoutes } from "./modules/auth/patientAuth/patientAuth.route";
-import { doctorAuthRoutes } from "./modules/auth/doctorAuth/doctorAuth.route";
 import { outletRoutes } from "./modules/outlet/outlet.route";
 import { outletUserRoutes } from "./modules/outlet_user/outlet_user.route";
 import { roleRoutes } from "./modules/role/role.route";
 import { permissionRoutes } from "./modules/permission/permission.route";
-import { outletAuthRoutes } from "./modules/auth/outlet_user_auth/outlet_user_auth.route";
 import { superAdminRoutes } from "./modules/super_admin/super_admin.route";
 import { superAdminRoleRoutes } from "./modules/super_admin_role/super_admin_role.route";
 import { HomeBannerRoutes } from "./modules/website_content/home/banner/banner.route";
@@ -40,13 +37,6 @@ const appRouter = Router();
 const moduleRoutes = [
   { path: "/patients", route: patientRoutes },
   { path: "/auth", route: authRoutes },
-
-
-  { path: "/auth/patients", route: patientAuthRoutes },
-  { path: "/auth/doctors", route: doctorAuthRoutes },
-  {
-    path: "/auth/outlet-users", route: outletAuthRoutes
-  },
 
   { path: "/doctors", route: doctorRoutes },
   { path: "/doctor-specializations", route: doctorSpecializationRoutes },
